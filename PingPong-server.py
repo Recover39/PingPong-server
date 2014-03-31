35bc5ae224f1cc70c4b3494dc8c292c5b937202f
reaperes <reaperes105@gmail.com>
2014-03-31 14:00:57 +0900

    fix minor bug

--- PingPong-server.py
+++ PingPong-server.py
@@ -8,19 +8,6 @@ app = Flask(__name__)
 push_worker = PushWorker.PushThread()
 
 
-def main(args):
-    if args[0] == 'start':
-        print 'push_worker start'
-        push_worker.start()
-        print 'server start'
-        app.run(threaded=True)
-
-    elif args[0] == 'stop':
-        push_worker.stop_thread()
-        push_worker.join(100)
-        print 'server is terminated'
-
-
 @app.route('/', methods=['POST'])
 def index():
     request_data = request.get_json()
@@ -37,5 +24,7 @@ def index():
 
 
 if __name__ == '__main__':
-    import sys
-    main(sys.argv[1:])
+    print 'push_worker start'
+    push_worker.start()
+    print 'server start'
+    app.run(threaded=True)
--- test.py
+++ test.py
@@ -2,12 +2,12 @@
 import requests
 import json
 
-load = {"sender": "Namhoon", "receiver": "Sugarpoint", "messageData": "Let the game begin"}
-print(load)
-# r = requests.post('http://10.73.45.162:5000', data=json.dumps(load), headers={"Content-Type": "application/json"})
-r = requests.post('http://127.0.0.1:5000', data=json.dumps(load), headers={"Content-Type": "application/json"})
-print(r.status_code)
-print(r.text)
+# load = {"sender": "Namhoon", "receiver": "Sugarpoint", "messageData": "Let the game begin"}
+# print(load)
+# # r = requests.post('http://10.73.45.162:5000', data=json.dumps(load), headers={"Content-Type": "application/json"})
+# r = requests.post('http://127.0.0.1:5000', data=json.dumps(load), headers={"Content-Type": "application/json"})
+# print(r.status_code)
+# print(r.text)
 
 # from model.GCMMessage import Message
 #
@@ -15,15 +15,15 @@ print(r.text)
 # print (message)
 
 
-# import traceback
-# from gcm import GCM
-#
-# gcm = GCM('AIzaSyC3CwECHoUqHKNQXbndpRWPjgauYhXTUEI')
-# import Contacts
-# id = Contacts.ids['sender']
-# data = {'messageData': 'you give me the money too'}
-#
-# try:
-#     gcm.plaintext_request(registration_id=id, data=data)
-# except:
-#     traceback.print_exc()
+import traceback
+from gcm import GCM
+
+gcm = GCM('AIzaSyC3CwECHoUqHKNQXbndpRWPjgauYhXTUEI')
+import Contacts
+id = Contacts.ids['Sugarpoint']
+data = {'messageData': 'kkk'}
+
+try:
+    gcm.plaintext_request(registration_id=id, data=data)
+except:
+    traceback.print_exc()
